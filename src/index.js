e6efaa7a2079df9011e8e44d059c2a567503bda0
Peter <petr_adamovich@i.ua>
2020-02-02 22:28:21 +0200

    21 lesson is over

--- src/index.js
+++ src/index.js
@@ -4,16 +4,19 @@ import ReactDOM from 'react-dom';
 import Routes from 'pages/routes'
 import {BrowserRouter as Router} from 'react-router-dom';
 import TopBar from 'components/topBar'
-import {CurrentUserProvider} from 'contexts/currentUser'
+import {CurrentUserProvider} from 'contexts/currentUser';
+import CurrentUserChecker from 'components/currentUserChecker'
 
 
 const App = () => {
     return (
         <CurrentUserProvider>
-            <Router>
-                <TopBar />
-                <Routes />
-            </Router>
+            <CurrentUserChecker>
+                <Router>
+                    <TopBar />
+                    <Routes />
+                </Router>
+            </CurrentUserChecker>
         </CurrentUserProvider>
     )
 }
